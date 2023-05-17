6775d6760564d8f84abe47c17a44e8c1ceae7d1d
David Avery <124400470+averydi@users.noreply.github.com>
2023-05-17 22:45:34 +0000

    initial push

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,12 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import CharacterDetail from "./component/characterDetail.js";
+import StarshipsDetail from "./component/StarshipsDetail";
+import VehiclesDetail from "./component/VehiclesDetail";
+import PlanetDetail from "./component/PlanetDetail";
+import SpeciesDetail from "./component/SpeciesDetail";
+import FilmDetail from "./component/FilmDetail";
+import CharacterOverview from "./component/CharacterOverview";
+import PlanetOverview from "./component/PlanetOverview";
+import FilmOverview from "./component/FilmOverview";
+import StarshipsOverview from "./component/StarshipOverview";
+import SpeciesOverview from "./component/SpeciesOverview";
+import VehicleOverview from "./component/VehicleOverview";
 
 //create your first component
 const Layout = () => {
@@ -23,9 +33,19 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route path ="/people/:peopleId" element={<CharacterDetail />} />
+						<Route path ="/starships/:shipId" element={<StarshipsDetail />} />
+						<Route path ="/vehicles/:vehicleId" element={<VehiclesDetail />} />
+						<Route path ="/planets/:planetId" element={<PlanetDetail />} />
+						<Route path ="/species/:speciesId" element={<SpeciesDetail />} />
+						<Route path ="/films/:filmId" element={<FilmDetail />} />
+						<Route path ="/people/" element={<CharacterOverview />} />
+						<Route path ="/planets/" element={<PlanetOverview />} />
+						<Route path ="/films/" element={<FilmOverview />} />
+						<Route path ="/starships/" element={<StarshipsOverview />} />
+						<Route path ="/species/" element={<SpeciesOverview />} />
+						<Route path ="/vehicles/" element={<VehicleOverview />} />
 					</Routes>
 					<Footer />
 				</ScrollToTop>
